643db27581860ee130015b44d42a0849d3fadea4
vanessa <vanessa.chaillou@orange.fr>
2017-11-16 13:32:38 +0100

    ex js ex0

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("javascript")
+function add (a,b){
+	return a+b;
+}
+console.log(add (10,30));
+
+function sub(a,b){
+	return a-b;
+}
+console.log(sub(10,30));
+
+function divice(a,b){
+	if (b===0) {
+	return "essaie encore" 
+	} else {
+	return a/b;
+}
+}
+console.log(divice(10,30));
+
+function multiply (a,b){
+	return a*b;
+}
+console.log(multiply(3,5));
